a9aef9f6e954cd641893bd22deabecba91b60f97
acr304 <sulaiman.malik@haw-hamburg.de>
2023-06-27 21:51:24 +0200

    fixed WS-F sort when bot Ramp Blocked

--- main/src/logic/main_fsm/states/Running.cpp
+++ main/src/logic/main_fsm/states/Running.cpp
@@ -105,11 +105,14 @@ bool Running::master_LBW_Blocked()
 		WorkpieceType detected_type = wp->M_type;
 		WorkpieceType config_type = data->wpManager->getNextWorkpieceType();
 
-		if(!data->wpManager->getRamp_one() && data->wpManager->getRamp_two()) //compare()
+		bool rampOneBlocked= data->wpManager->getRamp_one();
+		bool rampTwoBlocked= data->wpManager->getRamp_two();
+
+		if(!rampOneBlocked && rampTwoBlocked) //compare()
 		{
 			wp->sortOut = detected_type != config_type;
 		}
-		else if (data->wpManager->getRamp_one() && !data->wpManager->getRamp_two())
+		else if ((rampOneBlocked && !rampTwoBlocked)||(rampOneBlocked && rampTwoBlocked))
 		{
 			wp->sortOut = false;
 		}
